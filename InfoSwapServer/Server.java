9cb832f419308a9524438609f4f926fa613ea727
CasualQuasar <93143332+Quasardilla@users.noreply.github.com>
2023-05-20 08:23:09 -0400

    experimenting with sockets

---
+++ InfoSwapServer/Server.java
@@ -0,0 +1,98 @@
+package InfoSwapServer;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class Server {
+    protected String host;
+    protected int port;
+
+    protected ServerSocket serverSocket;
+    protected Socket clientSocket;
+    protected DataInputStream in;
+    protected DataOutputStream out;
+
+    public Server(String host, int port) {
+        this.host = host;
+        this.port = port;
+        
+        try {
+            serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress(host, port));
+        } catch (IOException e) {}
+    }
+
+    public void listenForClient() {
+        try {
+            clientSocket = serverSocket.accept();
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+
+            Thread t = new ClientHandler(clientSocket, clientSocket.getInputStream(), clientSocket.getOutputStream());
+
+            t.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String listenForInput() throws IOException {
+        String input = "";
+        try {
+            input = in.readUTF();
+        } catch (SocketException e) {
+            System.out.println("Client Disconnected");
+            stop();
+            return "LOST CONNECTION";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return input;
+    }
+
+    public void send(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Server server = new Server("localhost", 5100);
+        server.listenForClient();
+        while (true) {
+            try {
+                String input = server.listenForInput();
+                if (input.equals("LOST CONNECTION")) {
+                    break;
+                }
+                System.out.println(input);
+                server.send("Hello Client");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- SimpleGreetingServer/Client.java
+++ SimpleGreetingServer/Client.java
@@ -1,3 +1,4 @@
+package SimpleGreetingServer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,15 +7,16 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Hashtable;
+import java.util.Scanner;
 
 import javax.naming.NamingException;
 import javax.naming.directory.Attributes;
 import javax.naming.directory.InitialDirContext;
 
 public class Client {
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
+    private static Socket clientSocket;
+    private static PrintWriter out;
+    private static BufferedReader in;
 
     public void startConnection(String ip, int port) {
         try {
@@ -27,7 +29,7 @@ public class Client {
         }
     }
 
-    public String sendMessage(String msg) throws IOException {
+    public static String sendMessage(String msg) throws IOException {
         out.println(msg);
         String resp = in.readLine();
         return resp;
@@ -41,22 +43,40 @@ public class Client {
     }
 
     public static void main(String[] args) {
-        try {
-            messageServer();
-        } catch (IOException e) {} catch (NamingException e) {}
+        Scanner sc = new Scanner(System.in);
+        initConnection();
+
+        String response = "";
+
+        while(!response.equals("hello client")) {
+            
+            
+            String input = sc.nextLine();
+            System.out.println("Sending " + input);
+
+            try {
+                response = sendMessage(input);
+                response = sendMessage(input + "extra");
+                response = sendMessage(input + "extra1");
+                response = sendMessage(input + "extra2");
+            } catch (IOException e) {}
+
+            if(response == null) {
+                System.out.println("Socket closed");
+                return;
+            }
+
+            System.out.println("Received " + response);
+
+        }
     }
 
-    public static void messageServer() throws IOException, NamingException {
+    public static void initConnection() {
         Client client = new Client();
         int port = 5100;
         String ip = "rottinger.net";
-        
 
         System.out.println("Connecting to " + ip + " on port " + port);
-
-        // client.startConnection("rottinger.net", 5100);
         client.startConnection(ip, port);
-        String response = client.sendMessage("hello server");
-        System.out.println(response);
     }
 }
